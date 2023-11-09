7ac16f906407d73de92d310c904878da28fd99c7
sky-cf <134393309+sky-cf@users.noreply.github.com>
2023-11-09 19:09:38 +0530

    module-update

--- main.tf
+++ main.tf
@@ -39,8 +39,8 @@ module "blog_sg"{
   ingress_rules = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0]
 
-  ingress_rules = ["all-all"]
-  ingress_cidr_blocks = ["0.0.0.0/0]
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0]
 }
 
 resource "aws_security_group" "blog"{
